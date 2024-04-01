efc04b37a096e0e1a11ba293c67a43dbdc7727a4
Evgen201097 <165675840+Evgen201097@users.noreply.github.com>
2024-04-01 21:28:53 +0300

    Homework commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to my development world!");
+        System.out.println("Добро пожаловать в мой мир разработки!");
+    }
+}
