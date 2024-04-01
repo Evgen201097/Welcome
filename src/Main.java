2997dd0c9d4e6e2e7d636be40f796ea8616ea998
Evgen201097 <165675840+Evgen201097@users.noreply.github.com>
2024-04-01 21:53:40 +0300

    Homework

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to my development world!");
         System.out.println("Добро пожаловать в мой мир разработки!");
+        System.out.println("Welcome to my world, the world of Shinigami!");
+        System.out.println("Добро пожаловать в мой мир, мир Шинигами!");
     }
 }
